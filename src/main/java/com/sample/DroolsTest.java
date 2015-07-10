30b577ee3ca36b1df4d6b1defd87d2675dce869c
Gaohaoyang <gaohaoyang126@126.com>
2015-07-10 15:44:00 +0800

    forward chain test done

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -18,40 +18,12 @@ public class DroolsTest {
 
             // go !
             Message message = new Message();
-            message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
+            message.setA2(true);
             kSession.insert(message);
             kSession.fireAllRules();
+            System.out.println(message.toString());
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
-
-    public static class Message {
-
-        public static final int HELLO = 0;
-        public static final int GOODBYE = 1;
-
-        private String message;
-
-        private int status;
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-    }
-
 }
